2e4e9997b1755fa4570265e189ec49412a6c7d8f
AlexMorvi <ariel.morales@epn.edu.ec>
2022-02-14 13:32:01 -0500

    Agregar archivos de proyecto.

---
+++ Inicio.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Asadero_Pollitos_En_Fuga
+{
+    public partial class Inicio : Form
+    {
+        public Inicio()
+        {
+            InitializeComponent();
+        }
+    }
+}
